a996b71b159066d21f23309b506279d5fec85174
DomeniGeco <domenigeco@gmail.com>
2026-05-15 17:49:00 +0200

    Update shortcuts.py

--- dsite/shortcuts.py
+++ dsite/shortcuts.py
@@ -1,1 +1,11 @@
 
+from .templates import render_template
+from .response import Response
+
+
+def render(request, template_name, context=None):
+    if context is None:
+        context = {}
+
+    html = render_template(template_name, context)
+    return Response(html)
