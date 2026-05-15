3c9982333377d41c8d8740b7b0748608bdc3974b
DomeniGeco <domenigeco@gmail.com>
2026-05-15 17:49:17 +0200

    Update forms.py

--- dsite/forms.py
+++ dsite/forms.py
@@ -1,1 +1,15 @@
+class Field:
+    def __init__(self, label=""):
+        self.label = label
 
+
+class TextField(Field):
+    pass
+
+
+class Form:
+    def __init__(self):
+        self.data = {}
+
+    def is_valid(self):
+        return True
