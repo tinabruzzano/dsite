9ee250054a794f573443de2006001af6bc4bcb17
tinabruzzano <tinabruzzano@gmail.com>
2026-05-16 11:52:34 +0200

    Update migrations.py

--- dsite/management/migrations.py
+++ dsite/management/migrations.py
@@ -1,12 +1,21 @@
 # dsite/management/migrations.py
 
+
 from dsite.db import Model
+from pathlib import Path
+import subprocess
 
 
 def makemigrations(project_name):
     print(f"[DSite] makemigrations per {project_name}")
-    print("[DSite] (placeholder) nessuna migrazione reale ancora")
-
+    migration = "migration.tar.gz"
+    fileofmig = Path(migration)
+    MIG_ROOT = Path(__file__)
+    subprocess.run(f"tar -czfd {fileofmig} *")
+    subprocess.rub("mkdir project_migrations")
+    subprocess.run(f"cp {fileofmig} project_migrations/")
+    
+    
 
 def migrate(project_name):
     print(f"[DSite] migrate per {project_name}")
