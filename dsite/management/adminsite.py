a4e051c5547e41d3359e09831dc0c7553690dd86
DomeniGeco <domenigeco@gmail.com>
2026-05-16 07:46:50 +0200

    Create adminsite.py

---
+++ dsite/management/adminsite.py
@@ -0,0 +1,8 @@
+# dsite/management/adminsite.py
+
+def adminsite(project_name):
+
+    print(f"[DSite] AdminSite avviato per {project_name}")
+    print("[DSite] http://127.0.0.1:8000/admin")
+
+    print("[DSite] (placeholder admin dashboard)")
