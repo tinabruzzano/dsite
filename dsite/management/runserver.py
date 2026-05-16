036451b02b4c88a7f0781fa6c472000558baf745
DomeniGeco <domenigeco@gmail.com>
2026-05-16 07:46:03 +0200

    Update runserver.py

--- dsite/management/runserver.py
+++ dsite/management/runserver.py
@@ -1,6 +1,7 @@
-# dsite/management/runserver.py
-
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from dsite.http.router import resolve
+from dsite.http.request import Request
 from dsite.template import render_template
 
 
@@ -8,7 +9,21 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        content = render_template("homepage.xml")
+        view = resolve(self.path)
+
+        if view is None:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+            return
+
+        request = Request("GET", self.path)
+
+        response = view(request)
+
+        content = render_template(
+            response.content if hasattr(response, "content") else response
+        )
 
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -19,7 +34,7 @@ class Handler(BaseHTTPRequestHandler):
 
 def runserver(project_name):
 
-    print(f"[DSite] Server avviato per {project_name}")
+    print(f"[DSite] Server avviato ({project_name})")
     print("[DSite] http://127.0.0.1:8000")
 
     server = HTTPServer(("127.0.0.1", 8000), Handler)
